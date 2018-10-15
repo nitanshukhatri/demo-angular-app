19320d1d4fb5bee20508a63e71583c0aa7840412
NitanshuKhatri <nitanshu.khatri@rsystems.com>
2018-10-15 19:10:47 +0530

    making common material module

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -4,30 +4,24 @@ import { CommonModule } from '@angular/common';
 import { FormRoutingModule } from './form-routing.module';
 import { SubscriptionFormComponent } from './features/subscription-form/subscription-form.component';
 import { FormComponent } from './form.component';
-import { MatCardModule, MatFormFieldModule, MatInputModule,
-   MatToolbarModule, MatSelectModule, MatDialogModule,
-    MatButtonModule, MatTableModule, MatSortModule, MatPaginatorModule } from '@angular/material';
+import {
+  MatCardModule, MatFormFieldModule, MatInputModule,
+  MatToolbarModule, MatSelectModule, MatDialogModule,
+  MatButtonModule, MatTableModule, MatSortModule, MatPaginatorModule
+} from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ExitComponent } from './features/exit/exit.component';
 import { ViewComponent } from './features/view/view.component';
 import { CoreModule } from '../core/core.module';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormRoutingModule,
-    MatCardModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatToolbarModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule,
-    CoreModule
+    CoreModule,
+    MaterialModule
   ],
   declarations: [
     FormComponent,
